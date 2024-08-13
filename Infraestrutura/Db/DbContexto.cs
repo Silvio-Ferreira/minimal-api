900a489639e365568471cc626c99850adc423d04
Silvio-Ferreira <150188262+Silvio-Ferreira@users.noreply.github.com>
2024-08-12 21:14:24 -0300

    Atualização
    Conexão com mysql

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -1,13 +1,30 @@
 using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
 
 namespace MinimalApi.Infraestrutura.Db;
 public class DbContexto : DbContext
 {
+    private readonly IConfiguration _configuracaoAppSetings;
+    public DbContexto(IConfiguration configuracaoAppSetings)
+    {
+            _configuracaoAppSetings = configuracaoAppSetings;
+    }
     public DbSet<Administrador> Administradores {get; set;} = default!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseMySql("string de conexão", ServerVersion.AutoDetect("string de conexão"));
-        //configurando o projeto min 17:10
+        if(!optionsBuilder.IsConfigured)
+        {
+
+
+            var stringConexao = _configuracaoAppSetings.GetConnectionString("mysql")?.ToString();
+            if(!string.IsNullOrEmpty(stringConexao))
+            {
+                optionsBuilder.UseMySql(stringConexao, 
+                ServerVersion.AutoDetect(stringConexao)
+             );
+            }
+       
+        }
     }
 }
