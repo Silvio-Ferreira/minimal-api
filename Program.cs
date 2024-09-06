06ed835c0d45cdac3962edf07572df500aa71023
Silvio-Ferreira <150188262+Silvio-Ferreira@users.noreply.github.com>
2024-09-05 22:11:24 -0300

    Update
    Configuração do Swagger para a passagem do token JWT

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using System.Security.Claims;
 using System.IdentityModel.Tokens.Jwt;
+using Microsoft.OpenApi.Models;
 
 
 
@@ -19,18 +20,21 @@ using System.IdentityModel.Tokens.Jwt;
 var builder = WebApplication.CreateBuilder(args);
 
 var key = builder.Configuration.GetSection("Jwt").ToString();
-if(string.IsNullOrEmpty(key)) key = "123456";
+if (string.IsNullOrEmpty(key)) key = "123456";
 
 
-builder.Services.AddAuthentication(option =>{
+builder.Services.AddAuthentication(option =>
+{
     option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(option => {
-     option.TokenValidationParameters = new TokenValidationParameters{
-                ValidateLifetime = true,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
-                ValidateIssuer = false,
-                ValidateAudience = false,
+}).AddJwtBearer(option =>
+{
+    option.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateLifetime = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+        ValidateIssuer = false,
+        ValidateAudience = false,
     };
 });
 
@@ -41,6 +45,31 @@ builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "Jwt",
+        In = ParameterLocation.Header,
+        Description = "Insira o token Jwt aqui"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement{
+       {
+         new OpenApiSecurityScheme{
+            Reference = new OpenApiReference{
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        },
+        new string[] {}
+       }
+    });
+});
 
 builder.Services.AddDbContext<DbContexto>(options =>
 {
@@ -53,73 +82,55 @@ var app = builder.Build();
 #endregion
 
 #region Home
-app.MapGet("/", () => Results.Json(new Home())).WithTags("Home");
+app.MapGet("/", () => Results.Json(new Home())).AllowAnonymous().WithTags("Home");
 #endregion
 
 #region  Administradores
-  string GerarTokenJwt(Administrador administrador){
-                if(string.IsNullOrEmpty(key)) return string.Empty;
+string GerarTokenJwt(Administrador administrador)
+{
+    if (string.IsNullOrEmpty(key)) return string.Empty;
 
-                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
-                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
-                var claims = new List<Claim>()
+    var claims = new List<Claim>()
                 {
                     new Claim ("Email", administrador.Email),
                     new Claim("Perfil", administrador.Perfil),
                 };
-                
-                var token = new JwtSecurityToken(
-                    claims: claims,
-                    expires: DateTime.Now.AddDays(1),
-                    signingCredentials: credentials
-                );
 
-                return new JwtSecurityTokenHandler().WriteToken(token);
-            }
+    var token = new JwtSecurityToken(
+        claims: claims,
+        expires: DateTime.Now.AddDays(1),
+        signingCredentials: credentials
+    );
 
-// string GerarTokenJwt(Administrador administrador){
-//     if (string.IsNullOrEmpty(key)) return string.Empty;
+    return new JwtSecurityTokenHandler().WriteToken(token);
+}
 
-//     var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
-//     var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
-//     var claims = new List<Claim>();
-//     {
-//         new Claim(ClaimTypes.Email, administrador.Email);
-//         new Claim("Perfil", administrador.Perfil);
-//     };
-
-//     var token = new JwtSecurityToken(
-//         claims: claims,
-//         expires: DateTime.Now.AddDays(1),
-//         signingCredentials: credentials
-//     );
-
-//     return new JwtSecurityTokenHandler().WriteToken(token);
-// }
 
 app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
     var adm = administradorServico.Login(loginDTO);
-                if(adm != null)
-                {
-                    string token = GerarTokenJwt(adm);
-                    return Results.Ok(new AdministradorLogado
-                    {
-                        Email = adm.Email,
-                        Perfil = adm.Perfil,
-                        Token = token
-                    });
-                }
-                else
-                    return Results.Unauthorized();
+    if (adm != null)
+    {
+        string token = GerarTokenJwt(adm);
+        return Results.Ok(new AdministradorLogado
+        {
+            Email = adm.Email,
+            Perfil = adm.Perfil,
+            Token = token
+        });
+    }
+    else
+        return Results.Unauthorized();
 
-}).WithTags("Administradores");
+}).AllowAnonymous().WithTags("Administradores");
 
 app.MapGet("/administradores", ([FromQuery] int? pagina, IAdministradorServico administradorServico) =>
 {
-    var adms = new List <AdministradorModelView>();
+    var adms = new List<AdministradorModelView>();
     var administradores = administradorServico.Todos(pagina);
     foreach (var adm in administradores)
     {
@@ -128,7 +139,7 @@ app.MapGet("/administradores", ([FromQuery] int? pagina, IAdministradorServico a
             Id = adm.Id,
             Email = adm.Email,
             Perfil = adm.Perfil
-           
+
         });
     }
     return Results.Ok(adms);
@@ -139,12 +150,12 @@ app.MapGet("/administradores/{id}", ([FromRoute] int id, IAdministradorServico a
     var administrador = administradorServico.BuscaPorId(id);
     if (administrador == null) return Results.NotFound();
     return Results.Ok(new AdministradorModelView
-        {
-            Id = administrador.Id,
-            Email = administrador.Email,
-            Perfil = administrador.Perfil
-           
-        });
+    {
+        Id = administrador.Id,
+        Email = administrador.Email,
+        Perfil = administrador.Perfil
+
+    });
 }).RequireAuthorization().WithTags("Administradores");
 
 app.MapPost("/administradores", ([FromBody] AdministradorDTO administradorDTO, IAdministradorServico administradorServico) =>
@@ -174,13 +185,13 @@ app.MapPost("/administradores", ([FromBody] AdministradorDTO administradorDTO, I
     };
     administradorServico.Incluir(administrador);
 
-    return Results.Created($"/administrador/{administrador.Id}",new AdministradorModelView
-        {
-            Id = administrador.Id,
-            Email = administrador.Email,
-            Perfil = administrador.Perfil
-           
-        });
+    return Results.Created($"/administrador/{administrador.Id}", new AdministradorModelView
+    {
+        Id = administrador.Id,
+        Email = administrador.Email,
+        Perfil = administrador.Perfil
+
+    });
 
 
 }).RequireAuthorization().WithTags("Administradores");
--- obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57c07ded9c5c20ce867f2dbb98539bfd89869f8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1be751477a19ea3724a779e89b38dd903ae9282")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
